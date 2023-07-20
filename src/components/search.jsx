04e2c3c8cb3987b6c02e0b0bbb4ea4038c591349
dekin <dekinlinux@gmail.com>
2023-07-20 15:49:54 +0000

    still fetch error

--- src/components/search.jsx
+++ src/components/search.jsx
@@ -1,38 +1,38 @@
-import React,{useState} from 'react'
-import {useNavigate} from 'react-router-dom'
+// import React,{useState} from 'react'
+// import {useNavigate} from 'react-router-dom'
 
-function Search() {
-    const [code,setCode] = useState('')
-    const navigate = useNavigate()
+// function Search() {
+//     const [code,setCode] = useState('')
+//     const navigate = useNavigate()
 
-    const submitCode = (e)=>{
+//     const submitCode = (e)=>{
 
-        e.preventDefault()
+//         e.preventDefault()
 
-        fetch('/birth.json')
-        .then(res=>res.json())
-        .then(data=>{
-          let user =data.find(user =>user.entry_no === code)
-          if(user){
-            navigate(`/verify/${code}`)
-          }else{
-            alert('Invalid Code!')
-          }
+//         fetch('/birth.json')
+//         .then(res=>res.json())
+//         .then(data=>{
+//           let user =data.find(user =>user.entry_no === code)
+//           if(user){
+//             navigate(`/verify/${code}`)
+//           }else{
+//             alert('Invalid Code!')
+//           }
         
-        })
+//         })
         
-    }
+//     }
 
 
     
-  return (
-    <div>
-        <form onSubmit={submitCode}>
-            <input type="text" placeholder='Enter Number' onChange={e=>setCode(e.target.value)} />
-            <button type='submit'>Verify </button>
-        </form>
-    </div>
-  )
-}
+//   return (
+//     <div>
+//         <form onSubmit={submitCode}>
+//             <input type="text" placeholder='Enter Number' onChange={e=>setCode(e.target.value)} />
+//             <button type='submit'>Verify </button>
+//         </form>
+//     </div>
+//   )
+// }
 
-export default Search
+// export default Search
--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,6 +1,5 @@
 import React from 'react'
 import Search from '../components/SearchBirth'
-import Birth from '../components/Birth'
 
 function Homepage() {
   return (
