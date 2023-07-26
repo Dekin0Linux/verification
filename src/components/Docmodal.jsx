69a777300991efd50cf05e97a508dd80f51f6b46
Kofi Bediako-Mensah Boadi <boadikofi466@gmail.com>
2023-07-26 11:02:57 +0000

    Search-input changes

--- src/components/Docmodal.jsx
+++ src/components/Docmodal.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
+import React from 'react';
 import { Modal, Button } from 'react-bootstrap';
 import Doc from '../pages/Doc';
 
-function Docmodal({ show, onHide }) {
+function Docmodal({ lgshow, onHide }) {
   return (
     <div>
-         <Modal show={show} onHide={onHide}>
-      <Modal.Header closeButton>
-        <Modal.Title>My Modal</Modal.Title>
-      </Modal.Header>
-      <Modal.Body>
-        <Doc/>
-      </Modal.Body>
-      <Modal.Footer>
-        <Button variant="secondary" onClick={onHide}>
-          Close
-        </Button>
-      </Modal.Footer>
-    </Modal>
+      <Modal size="lg" show={lgshow} onHide={onHide} aria-labelledby="example-modal-sizes-title-lg" dialogClassName="modal-90w" >
+        <Modal.Header closeButton>
+          <Modal.Title>My Modal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Doc />
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={onHide}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </div>
-  )
+  );
 }
 
-export default Docmodal
+export default Docmodal;
